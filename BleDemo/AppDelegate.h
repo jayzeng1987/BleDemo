0d1d8a142a7c0ba14f4a477eef9267f27cb54e9c
JayZ <jayzeng1987@163.com>
2016-04-14 14:23:48 +0800

    First commit

---
+++ BleDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BleDemo
+//
+//  Created by JayZ on 16/4/14.
+//  Copyright © 2016年 genvict. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
